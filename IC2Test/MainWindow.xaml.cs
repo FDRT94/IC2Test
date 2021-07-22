3060dd9857120185e814937cb7c304f662c16175
Frank de Raadt <31472893+FDRT94@users.noreply.github.com>
2021-07-22 06:43:20 +0200

    Pre Validation Commit

--- IC2Test/MainWindow.xaml.cs
+++ IC2Test/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,28 +21,32 @@ namespace IC2Test
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
-            McDataGrid.ItemsSource = LoadCollectionData();
+            TopDataGrid.ItemsSource = LoadCollectionData();
         }
 
-        // fill dataGrid with data
-        private List<Person> LoadCollectionData()
+
+        // fill TopdataGrid with data
+        private ObservableCollection<Person> LoadCollectionData()
         {
-            List<Person> Persons = new List<Person>();
+            ObservableCollection<Person> PersonList = new ObservableCollection<Person>();
 
             // no DateTime Mentioned because it is left empty at start
-            Persons.Add(new Person()
+            PersonList.Add(new Person()
             {
                 ID = 1,
                 Name = "Mark Rutte",
                 From = "Den Haag",
                 Household = 1,
-                
+                LastVisited = DateTime.Now
             });
 
-            Persons.Add(new Person()
+
+            PersonList.Add(new Person()
             {
                 ID = 2,
                 Name = "Geert Wilders",
@@ -49,7 +54,7 @@ namespace IC2Test
                 Household = 1,
             });
 
-            Persons.Add(new Person()
+            PersonList.Add(new Person()
             {
                 ID = 3,
                 Name = "Wopke Hoekstra",
@@ -57,7 +62,7 @@ namespace IC2Test
                 Household = 1,
             });
 
-            Persons.Add(new Person()
+            PersonList.Add(new Person()
             {
                 ID = 4,
                 Name = "Willem van Alexander",
@@ -65,7 +70,7 @@ namespace IC2Test
                 Household = 1,
             });
 
-            Persons.Add(new Person()
+            PersonList.Add(new Person()
             {
                 ID = 5,
                 Name = "Jane Doe",
@@ -73,7 +78,7 @@ namespace IC2Test
                 Household = 2,
             });
 
-            Persons.Add(new Person()
+            PersonList.Add(new Person()
             {
                 ID = 6,
                 Name = "John Doe",
@@ -81,7 +86,7 @@ namespace IC2Test
                 Household = 2,
             });
 
-            Persons.Add(new Person()
+            PersonList.Add(new Person()
             {
                 ID = 7,
                 Name = "Denzel Dumfries",
@@ -89,7 +94,7 @@ namespace IC2Test
                 Household = 3,
             });
 
-            Persons.Add(new Person()
+            PersonList.Add(new Person()
             {
                 ID = 8,
                 Name = "Matthijs de Ligt",
@@ -97,7 +102,7 @@ namespace IC2Test
                 Household = 3,
             });
 
-            Persons.Add(new Person()
+            PersonList.Add(new Person()
             {
                 ID = 9,
                 Name = "Donyell Malen",
@@ -105,7 +110,7 @@ namespace IC2Test
                 Household = 3,
             });
 
-            Persons.Add(new Person()
+            PersonList.Add(new Person()
             {
                 ID = 10,
                 Name = "Marten de Roon",
@@ -113,59 +118,100 @@ namespace IC2Test
                 Household = 3,
             });
 
-            return Persons;
+            return PersonList;
         }
 
+        // Terras Regels
 
-        private void btnShowSelectedItem_Click(object sender, RoutedEventArgs e)
-        {
-            foreach (object o in McDataGrid.SelectedItems)
-                try
-                {
-                    MessageBox.Show((o as Person).Name, "Kopje", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-                }
-                catch (Exception ex2)
-                {
-                    MessageBox.Show("Error" + ex2.Message.ToString(), "", MessageBoxButton.OK, MessageBoxImage.Error);
-                    return;
-                }
-        }
-
-        private void btnSelectLast_Click(object sender, RoutedEventArgs e)
-        {
-            McDataGrid.SelectedIndex = McDataGrid.Items.Count - 1;
-        }
-
-        private void btnSelectNext_Click(object sender, RoutedEventArgs e)
-        {
-            int nextIndex = 0;
-            if ((McDataGrid.SelectedIndex >= 0) && (McDataGrid.SelectedIndex < (McDataGrid.Items.Count - 1)))
-                nextIndex = McDataGrid.SelectedIndex + 1;
-            McDataGrid.SelectedIndex = nextIndex;
-        }
-
-        private void btnSelectAll_Click(object sender, RoutedEventArgs e)
-        {
-            foreach (object o in McDataGrid.Items)
-                McDataGrid.SelectedItems.Add(o);
-        }
+        //private void Validation
 
         // Terras Options
-        
 
-        private void btnAddPersonTerras_Click(object sender, RoutedEventArgs e)
+        // selecteer item dat verplaatst moet worden
+        // voeg Bezoeker toe aan locatie
+        // Geef LastVisited een Waarde
+        // error handling
+        private void NaarTerras(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("hello world", "Kopje", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-        }
-        private void btnRemovePersonTerras_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("hello world", "Kopje", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-        }
-        private void btnRemoveAllTerras_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("hello world", "Kopje", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            // select selected item
+            foreach (object o in TopDataGrid.SelectedItems)
+            {
+                try
+                {
+                    var bezoeker = o as Person;
+
+                    TerrasDataGrid.Items.Add(bezoeker);
+
+                    for (int i = 0; i < TerrasDataGrid.Items.Count + 1; i++)
+                    {
+                        bezoeker.LastVisited = DateTime.Now;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error" + ex.Message.ToString(), "", MessageBoxButton.OK, MessageBoxImage.Error);
+                    throw;
+                }
+            }
         }
 
+        // verwijderd 1 bezoeker
+        // selecteer bezoeker
+        // verwijder bezoeker vanuit TerrasDatagrid
+        // error Handling
+        private void NaarHuis(object sender, RoutedEventArgs e)
+        {
+
+            //  foreach (object o in TopDataGrid.SelectedItems)
+
+            //var bezoeker = o as Person;
+
+            //TerrasDataGrid.Items.Add(bezoeker);
+
+            //for (int i = 0; i < TerrasDataGrid.Items.Count + 1; i++)
+            //{
+            //    bezoeker.LastVisited = DateTime.Now;
+            //}
+
+            //var bezoeker = o as Person;
+
+            //for (int i = 0; i < TerrasDataGrid.Items.Count + 1; i++)
+            //{
+            //    bezoeker.LastVisited = DateTime.Now;
+            //}
+
+            //try
+            //{
+            //    foreach (object o in TerrasDataGrid.SelectedItems)
+            //{
+            //        TerrasDataGrid.SelectedItem.Remove(o);
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("Error" + ex.Message.ToString(), "", MessageBoxButton.OK, MessageBoxImage.Error);
+            //    throw;
+            //}
+            
+
+        }
+
+        // Leegt het volledige Terras van alle bezoekers
+        // selecteer alle items
+        // Leeg volledig Terras
+        private void LeegTerras(object sender, RoutedEventArgs e)
+        {
+            // select alle items item
+            try
+            {
+                TerrasDataGrid.Items.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex.Message.ToString(), "", MessageBoxButton.OK, MessageBoxImage.Error);
+                throw;
+            }
+        }
 
         // Person.DateTime has ? because Datetime should be nullable at the start
         public class Person
@@ -175,12 +221,6 @@ namespace IC2Test
             public string From { get; set; }
             public int Household { get; set; }
             public DateTime? LastVisited { get; set; }
-
-        }
-
-        private void NaarTerras(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
